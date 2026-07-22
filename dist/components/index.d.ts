90021d0a50a9457ae1aba8e1d935cc7d8e1a838c
saberzero1 <github@emilebangma.com>
2026-07-23 00:40:18 +0200

    chore: migrate to npm publishing

--- dist/components/index.d.ts
+++ dist/components/index.d.ts
@@ -1,2 +1,10 @@
-export { Search, SearchField, SearchOptions } from '../index.js';
-import '@quartz-community/types';
+import { QuartzComponent } from '@quartz-community/types';
+
+type SearchField = "title" | "content" | "tags";
+interface SearchOptions {
+    enablePreview: boolean;
+    fieldPriority: SearchField[];
+}
+declare const _default: (userOpts?: Partial<SearchOptions>) => QuartzComponent;
+
+export { _default as Search, type SearchField, type SearchOptions };
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,2 @@
-import { QuartzComponent } from '@quartz-community/types';
-
-type SearchField = "title" | "content" | "tags";
-interface SearchOptions {
-    enablePreview: boolean;
-    fieldPriority: SearchField[];
-}
-declare const _default: (userOpts?: Partial<SearchOptions>) => QuartzComponent;
-
-export { _default as Search, type SearchField, type SearchOptions };
+export { Search, SearchField, SearchOptions } from './components/index.js';
+import '@quartz-community/types';
